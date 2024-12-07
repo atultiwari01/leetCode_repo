c98552a506a4a15a9abbfb8d76ab10427c486400
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2024-12-07 08:58:02 +0530

    Time: 2 ms (90.26%), Space: 42 MB (7.02%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,24 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
-    public int heightChecker(int[] heights) {
+    public static int heightChecker(int[] heights){
         int[] sortHeight = heights.clone();
-        int n = sortHeight.length;
-        for(int i = 0 ; i < n-1 ; i++){
-            for(int j = 0 ; j < n-i-1 ; j++){
-                if(sortHeight[j] == sortHeight[j+1]){
-                    continue;
-                }else if (sortHeight[j]>sortHeight[j+1]){
-                    int temp =sortHeight[j];
-                    sortHeight[j] = sortHeight[j+1];
-                    sortHeight[j+1] = temp;
-                }
-            }
-        }
+        Arrays.sort(sortHeight);
         int count = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(sortHeight[i]!=heights[i]){
+        for (int i = 0; i < heights.length; i++) {
+            if (sortHeight[i] != heights[i]) {
                 count++;
             }
         }
+        
         return count;
     }
-} 
+}
