821cb829887b36dfe3660a0d5948a7499afea7f3
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2024-12-21 12:16:53 +0530

    Time: 1 ms (73%), Space: 40.6 MB (64.83%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    boolean isHappy(int n) {
+        Set<Integer> happy = new HashSet<>();
+        while (true) {
+
+            int sum = 0;
+            while (n != 0) {
+                sum += Math.pow(n % 10, 2);
+                n = n / 10;
+            }
+            if (sum == 1) return true;
+
+            n = sum;
+
+            if (happy.contains(n))
+                return false;
+            happy.add(n);
+        }
+    }
+}
