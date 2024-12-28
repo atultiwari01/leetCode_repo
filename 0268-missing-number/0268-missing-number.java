ab0a4780e9e40dce41675e72be6f1d857dcd7a93
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2024-12-28 12:44:35 +0530

    Time: 6 ms (12.56%), Space: 44.9 MB (95.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,14 +2,14 @@ class Solution {
     public int missingNumber(int[] nums) {
         int[] clone = nums.clone();
         Arrays.sort(clone);
-        if (clone.length == 1) {
-            return clone[0] == 0 ? 1 : 0;
+        if (clone[0] != 0) {
+            return 0;
         }
-        for(int i = 0; i<clone.length-1 ; i++){
-            if(clone[i+1]-clone[i]!=1){
-                return clone[i]+1;
+        for (int i = 0; i < clone.length - 1; i++) {
+            if (clone[i + 1] - clone[i] != 1) {
+                return clone[i] + 1;
             }
         }
-        return clone[clone.length-1]+1;
+        return clone[clone.length - 1] + 1;
     }
 }
