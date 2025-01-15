c3ffb2ca164876869d1a1929af06e431fc1c2b2b
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2025-01-16 00:57:09 +0530

    Time: 101 ms (21.77%), Space: 46 MB (8.3%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String minWindow(String s, String t) {
+         int[] map = new int[128];
+        char[] arr = s.toCharArray();
+        //Set up the table
+        for(char cur : t.toCharArray()){
+            map[cur]++;
+        }
+        
+        int countAllCharInT = 0;
+        int left = 0, n = arr.length, right = 0;
+        int minLen = Integer.MAX_VALUE;
+        String minLenStr = "";
+        
+        while(right < n){
+            map[arr[right]]--;
+            if(0 <= map[arr[right]]){
+                countAllCharInT++;         
+            }
+            
+            while(countAllCharInT == t.length()){
+                //Update the minLen
+                if(minLen > right - left + 1){
+                    minLen = right - left + 1;
+                    minLenStr = s.substring(left, right + 1);
+                }
+                
+                map[arr[left]]++;
+                if(0 < map[arr[left]]){
+                    countAllCharInT--;
+                }
+                left++;
+            }
+            
+            right++;
+        }
+        
+        return minLenStr;
+    }
+}
