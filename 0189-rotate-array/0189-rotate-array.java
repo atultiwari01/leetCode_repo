36e047b30bca3adac148c04e7caf3002c6ac5ccc
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2025-08-03 07:50:30 +0530

    Time: 1 ms (46.16%), Space: 57.5 MB (52.3%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,7 +2,7 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int l = nums.length;
         int[] arr = new int[l];
-        int j = l - k % l;  // safe even if k > l
+        int j = l - k % l;
 
         for (int i = 0; i < l; i++) {
             arr[i] = nums[j % l];
